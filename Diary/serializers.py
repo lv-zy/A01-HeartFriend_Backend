bc44fa15a8131f6d0e16b4748bad0bcd8cd8e35e
Kin <mkknightr@gmail.com>
2023-12-05 12:11:49 +0800

    Add feature : Diary-Upload-Image

--- Diary/serializers.py
+++ Diary/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
-from .models import Diary 
+from .models import Diary, Image
 from Authentication.models import User
 
+class urlListField(serializers.ListField):
+    def to_representation(self, obj):
+        return obj.split(',') if obj else []
+    def to_internal_value(self, data):
+        return ','.join(data) if data else ''
+
+
+
+
+
 class DiarySerializer(serializers.ModelSerializer):
-    
+    images = urlListField()
     class Meta: 
         model = Diary
         fields = '__all__'
@@ -18,3 +28,8 @@ class UserDiarySerializer(serializers.ModelSerializer):
         user_diary_list = Diary.objects.filter(author=self.context['request'].user)
         serializer = DiarySerializer(user_diary_list, many=True)
         return serializer.data
+    
+class ImageUploadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = '__all__'
